cac7c4a43005db43b85179d251a32bf0e750b3d3
sateras <satpekov02@mail.ru>
2023-02-15 01:42:54 +0600

    play + playButton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Grid from "./components/Grid";
 function App() {
   let rows = 30;
   let cols = 50;
+  let speed = 100;
+  const [generation, setGeneration] = useState(0);
   const [gridFull, setGridFull] = useState(
     Array(rows)
       .fill()
@@ -21,7 +23,7 @@ function App() {
     let copyGreed = [...gridFull];
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        if (Math.floor(Math.random()) * 4 === 1) {
+        if (Math.floor(Math.random() * 4) === 1) {
           copyGreed[i][j] = true;
         }
       }
@@ -29,11 +31,39 @@ function App() {
     setGridFull(copyGreed);
   };
 
+  const playButton = () => {
+    clearInterval(intervalId);
+    let intervalId = setInterval(play, speed);
+  };
+
+  const play = () => {
+    let g = gridFull;
+    let g2 = [...gridFull];
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < rows - 1) if (g[i + 1][j]) count++;
+        if (i < rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < rows - 1 && j < cols - 1) if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    setGridFull(g2);
+    setGeneration(generation + 1);
+  };
+
   return (
     <div>
-      <h1>The Game of life</h1>
+      <h1 onClick={play}>The Game of life</h1>
       <Grid selectBox={selectBox} cols={cols} gridFull={gridFull} rows={rows} />
-      <h2>Generation: </h2>
+      <h2>Generation: {generation}</h2>
     </div>
   );
 }
